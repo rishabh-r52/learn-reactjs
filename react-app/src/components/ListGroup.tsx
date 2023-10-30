7a0d7910e5c7b025d34564e242cd5957524ae659
Rishabh Ranjan <rishabh9050@gmail.com>
2023-10-30 15:49:40 +0530

    ListGroup

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,40 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = [
+    "The Last of Us",
+    "God Of War",
+    "The Witcher 3",
+    "Grand Theft Auto V",
+    "Assassin's Creed II",
+  ];
+
+  // items = [];
+  // if (items.length == 0) {
+  //   return <p>No element found</p>;
+  // }
+
+  // Message to be printed when items list is empty
+  const EmptyMessage = items.length === 0 && <p>No Elements Found!</p>;
+
+  // Item Click Event
+  const HandleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
+
+  // Message to be printed when items list is not empty
+  const NonEmptyMessage = items.map((item) => (
+    <li className="list-group-item" onClick={HandleClick} key={item}>
+      {item}
+    </li>
+  ));
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Items</h1>
+      {EmptyMessage}
+      <ul className="list-group">{NonEmptyMessage}</ul>
+    </>
   );
 }
 
